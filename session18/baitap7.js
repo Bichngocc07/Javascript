c290c84582cc61b931ce7b20bfb5aeee26bb1941
Bichngocc07 <ngoc01072006@gmail.com>
2025-03-03 00:18:01 +0700

    Update baitap7.js

---
+++ session18/baitap7.js
@@ -0,0 +1,10 @@
+let a = parseFloat(prompt("Mời bạn nhập vào số a"));
+let b = parseFloat(prompt("Mời bạn nhập vào số b"));
+let pheptoan = prompt("Mời bạn nhập vào phép tính (+, -, *, /)");
+let ketqua;
+if (pheptoan === "+") {
+    ketQua = a + b;
+} else if (pheptoan === "-") {
+    ketQua = a - b;
+} else if (pheptoan === "*") 
+    ketQua = a * b
