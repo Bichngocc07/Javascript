88c6a35276ab8d9f710e2936cf87c9fa5bc73ce1
Bichngocc07 <ngoc01072006@gmail.com>
2025-03-14 17:38:33 +0700

    Add files via upload

---
+++ session28/bai1.js
@@ -0,0 +1,14 @@
+let person={
+    name:"John Doe",
+    age:25,
+    job:"Developer",
+}
+console.log("Name: " + person.name);
+console.log("Age: " + person.age);
+console.log("Job: " + person.job);
+
+// xoa
+//delete TÊN ĐỐI TƯỢNG .THUỘC TÍNH
+// IN ra sau khi xóa
+//Console.log 
+//for_in 
